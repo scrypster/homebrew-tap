0d8794b85e25b8540400993b8f9acf7f871811ef
Scrypster <59214607+scrypster@users.noreply.github.com>
2026-03-06 09:36:57 -0500

    chore: update muninn to v0.3.7-alpha

--- Formula/muninn.rb
+++ Formula/muninn.rb
@@ -1,24 +1,24 @@
 class Muninn < Formula
   desc "Cognitive memory database — MCP-native, single binary"
   homepage "https://muninndb.com"
-  version "0.3.6-alpha"
+  version "0.3.7-alpha"
 
   on_macos do
     on_arm do
       url "https://github.com/scrypster/muninndb/releases/download/v#{version}/muninn-darwin-arm64"
-      sha256 "9dce508979dcbb655e16d7d948166fa7c0e558d801325399bec7d613c7e5cc5c"  # darwin-arm64
+      sha256 "9b9336fec384ecd283fe7de98e1b7482f347441cd4250a7f4c3becaea8853f3b"  # darwin-arm64
     end
 
     on_intel do
       url "https://github.com/scrypster/muninndb/releases/download/v#{version}/muninn-darwin-amd64"
-      sha256 "3ba294569fe6fa8ca2b6afe5eba6c4b8e275bd0508677430441fd4c4e02dc6bd"  # darwin-amd64
+      sha256 "e3196192879a536bd7e223b14362632de2c23b5022867727e66575cfdd2cca16"  # darwin-amd64
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/scrypster/muninndb/releases/download/v#{version}/muninn-linux-amd64"
-      sha256 "fb7e70ce9f73ae6a406e283f394588734e3a73e07d756a8ffdffcedd22953d24"  # linux-amd64
+      sha256 "f974b8d291cf33132b7125bad73411c398bd4b7cc972377c5f299cd896edd6ac"  # linux-amd64
     end
   end
 
